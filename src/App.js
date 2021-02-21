83e17c961d6e46ae685f592acf4f7509a631b0c5
sovsianykov <swebproger@gmail.com>
2021-02-21 10:08:19 +0200

    deff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-import MyCarousel from "./components/MyCarousel";
 import MyGallery from "./components/Gallery";
 
 function App() {
   return (
     <div className="App container-fluid">
-      {/*<MyCarousel/>*/}
+
       <MyGallery/>
     </div>
   );
