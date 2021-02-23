3e22455a11f0047c73dfddb2cbb46ca223de09e3
”sovsianykov” <swebproger@gmail.com>
2021-02-24 00:33:18 +0200

    first audio

--- src/components/Mosaic.jsx
+++ src/components/Mosaic.jsx
@@ -1,19 +1,25 @@
-import React, {Component} from "react";
+import React, {Component,useEffect} from "react";
 import music from '../land.mp3'
 
 import Sound from 'react-sound'
 
 
 class Mosaic  extends Component{
+    //
+    componentDidMount() {
+        const autoPlay= true
+      }
 
 
-        render() {
+    render() {
             return (
                 <Sound
                     url={music}
                     volume={50}
                     playStatus={Sound.status.PLAYING}
                     playFromPosition={300 /* in milliseconds */}
+                    autoLoad={true}
+                    autoPlay={true}
                     // onLoading={this.handleSongLoading}
                     // onPlaying={this.handleSongPlaying}
                     // onFinishedPlaying={this.handleSongFinishedPlaying}
